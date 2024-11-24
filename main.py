53685c8ab330875f7cdf1a0f85c2a0158df4d104
vincensiusadyatma <adyatmav@gmail.com>
2024-11-24 15:03:12 +0700

    change main again

--- main.py
+++ main.py
@@ -40,8 +40,8 @@ app.include_router(
     tags=["Healthcheck"]
 )
 
-app.include_router(
-    __import__("app.auth.auth", fromlist=["router"]).router, 
-    prefix="/auth", 
-    tags=["Auth"]
-)
+# app.include_router(
+#     __import__("app.auth.auth", fromlist=["router"]).router, 
+#     prefix="/auth", 
+#     tags=["Auth"]
+# )
