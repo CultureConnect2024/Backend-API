a1411258263f32f24560ac93557422424ca2e745
vincensiusadyatma <adyatmav@gmail.com>
2024-11-24 15:53:45 +0700

    fix authroutes cred

--- app/auth/authRoutes.py
+++ app/auth/authRoutes.py
@@ -1,39 +1,41 @@
 import firebase_admin
 from firebase_admin import credentials, auth
-from fastapi import HTTPException, APIRouter, Header
-import os
 from fastapi import HTTPException, APIRouter, Body
+import os
 
 router = APIRouter()
 
-# # find service_account.json
-# base_dir = os.path.dirname(os.path.abspath(__file__))  
-# cred_path = os.path.join(base_dir, '..', '..', 'firebase-account/service_account.json')
+# Mendapatkan path dari environment variable
+cred_path = os.getenv("FIREBASE_SERVICE_ACCOUNT_PATH", "/firebase-account/service_account.json")
 
-# # Initial firebase admin
-# cred = credentials.Certificate(cred_path)
-# firebase_admin.initialize_app(cred)
+# Memastikan file ada di path yang telah ditentukan
+if not os.path.exists(cred_path):
+    raise HTTPException(status_code=500, detail="Firebase service account file not found at the specified path")
 
-# def verify_firebase_token(id_token: str):
-#     try:
-#         decoded_token = auth.verify_id_token(id_token)
-#         return decoded_token["uid"]
-#     except Exception as e:
-#         raise HTTPException(status_code=401, detail="Invalid token")
+# Menginisialisasi Firebase Admin SDK dengan file service_account.json
+cred = credentials.Certificate(cred_path)
+firebase_admin.initialize_app(cred)
 
+# Fungsi untuk memverifikasi ID token
+def verify_firebase_token(id_token: str):
+    try:
+        decoded_token = auth.verify_id_token(id_token)
+        return decoded_token["uid"]
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Invalid token")
 
+# Endpoint untuk login
+@router.post("/login")
+async def login(request: dict = Body(...)):
+    try:
+        id_token = request.get('id-token')
+        decoded_token = auth.verify_id_token(id_token)
+        user_id = decoded_token["uid"]
+        return {"user_id": user_id}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
-
-# @router.post("/login")
-# async def login(request: dict = Body(...)):
-#     try:
-#         id_token = request.get('id-token')  
-#         decoded_token = auth.verify_id_token(id_token)
-#         user_id = decoded_token["uid"]
-#         return {"user_id": user_id}  
-#     except Exception as e:
-#         raise HTTPException(status_code=400, detail=str(e))
-
+# Root endpoint
 @router.get("/")
 def root():
-    return "<h1>Auth Routes</h2>"
+    return "<h1>Auth Routes</h1>"
