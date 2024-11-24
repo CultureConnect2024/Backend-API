a3baa06828ee0f6932d9acf57638967720ae27fc
vincensiusadyatma <adyatmav@gmail.com>
2024-11-24 15:35:08 +0700

    fix auth

--- app/auth/authRoutes.py
+++ app/auth/authRoutes.py
@@ -6,31 +6,34 @@ from fastapi import HTTPException, APIRouter, Body
 
 router = APIRouter()
 
-# find service_account.json
-base_dir = os.path.dirname(os.path.abspath(__file__))  
-cred_path = os.path.join(base_dir, '..', '..', 'firebase-account/service_account.json')
+# # find service_account.json
+# base_dir = os.path.dirname(os.path.abspath(__file__))  
+# cred_path = os.path.join(base_dir, '..', '..', 'firebase-account/service_account.json')
 
-# Initial firebase admin
-cred = credentials.Certificate(cred_path)
-firebase_admin.initialize_app(cred)
+# # Initial firebase admin
+# cred = credentials.Certificate(cred_path)
+# firebase_admin.initialize_app(cred)
 
-def verify_firebase_token(id_token: str):
-    try:
-        decoded_token = auth.verify_id_token(id_token)
-        return decoded_token["uid"]
-    except Exception as e:
-        raise HTTPException(status_code=401, detail="Invalid token")
-
-
+# def verify_firebase_token(id_token: str):
+#     try:
+#         decoded_token = auth.verify_id_token(id_token)
+#         return decoded_token["uid"]
+#     except Exception as e:
+#         raise HTTPException(status_code=401, detail="Invalid token")
 
 
-@router.post("/login")
-async def login(request: dict = Body(...)):
-    try:
-        id_token = request.get('id-token')  
-        decoded_token = auth.verify_id_token(id_token)
-        user_id = decoded_token["uid"]
-        return {"user_id": user_id}  
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
 
+
+# @router.post("/login")
+# async def login(request: dict = Body(...)):
+#     try:
+#         id_token = request.get('id-token')  
+#         decoded_token = auth.verify_id_token(id_token)
+#         user_id = decoded_token["uid"]
+#         return {"user_id": user_id}  
+#     except Exception as e:
+#         raise HTTPException(status_code=400, detail=str(e))
+
+@router.get("/")
+def root():
+    return "<h1>Auth Routes</h2>"
